1fbd93527214bd54deefd65919fb89bb3b2451dc
AlexisChebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2024-10-01 20:12:56 -0300

    cookie consent

---
+++ Frontend Projects/11-cookie-consent/script.js
@@ -0,0 +1,7 @@
+document.getElementById('close-button').addEventListener('click', function (){
+    document.getElementById('cookie-consent').style.display = 'none';
+});
+
+document.getElementById('cookie-accept').addEventListener('click', function (){
+    document.getElementById('cookie-consent').style.display = 'none';
+});
